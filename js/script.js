9fbc7b40ddd1ac729a2c179c7e9ae77ad23546f1
pikalupps <putudhikap@gmail.com>
2022-10-28 20:56:48 +0700

    fix trip page

--- js/script.js
+++ js/script.js
@@ -10,3 +10,33 @@ window.onscroll = () => {
 	menu.classList.remove('bx-x');
 	navbar.classList.remove('open');
 };
+
+const dropdowns = document.querySelectorAll('.dropdown');
+
+dropdowns.forEach(dropdown => {
+	const select = dropdown.querySelector('.select');
+	const caret = dropdown.querySelector('.caret');
+	const menu = dropdown.querySelector('.menu');
+	const options = dropdown.querySelector('.menu li');
+	const selected = dropdown.querySelector('.selected');
+
+	select.addEventListener('click', () => {
+		select.classList.toggle('select-clicked');
+		caret.classList.toggle('caret-rotate');
+		menu.classList.toggle('menu-open');
+	});
+
+	options.forEach(option => {
+		option.addEventListener('click', () =>{
+			selected.innerText = option.innerText;
+
+			select.classList.remove('select-clicked');
+			caret.classList.remove('caret-rotate');
+			menu.classList.remove('menu-open');
+
+			options.forEach(option => {
+				option.classList.add('active');
+			})
+		})
+	})
+})
